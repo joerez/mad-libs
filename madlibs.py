16ed5b0d451e964eba4c699c985f93592404dad0
Joerez <rezenbiz@gmail.com>
2018-08-29 15:13:40 -0700

    Let the dogs out to pee

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+# create a story here with variables as blanks
+
+
+# ask user for different words
+
+
+# show them the story
+
+
+'''
+Requirments:
+
+Users must fill out every word
+
+all user input must be strings
+
+'''
