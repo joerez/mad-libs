1856482a51ff1b2f93113fe12d10aec9ce43c19f
Joerez <rezenbiz@gmail.com>
2018-08-29 15:23:43 -0700

    yo dawg smell ya later

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,6 @@
+# ask user for different words
+
+
 noun1 = input("Give me a noun!")
 noun2 = input("Give me another noun!")
 verb1 = input("Give me a verb now!")
@@ -10,10 +13,8 @@ place1 = input("Give me a place!")
 story = "Now this is a story all about how My " + noun1 + " got flipped turned upside down And I'd like to take a " + noun2 + ", just " + verb1 + " right there. I'll tell you how I became the " + type_of_person + " of " + place1 + "."
 
 
-# ask user for different words
-
-print(story)
 # show them the story
+print(story)
 
 
 '''
