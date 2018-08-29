b1b4826f155c703273edf855c08588130392491e
Joerez <rezenbiz@gmail.com>
2018-08-29 15:23:07 -0700

    the deed is done

--- madlibs.py
+++ madlibs.py
@@ -1,9 +1,18 @@
+noun1 = input("Give me a noun!")
+noun2 = input("Give me another noun!")
+verb1 = input("Give me a verb now!")
+type_of_person = input("Give me a type of person!")
+place1 = input("Give me a place!")
+
+
 # create a story here with variables as blanks
+
+story = "Now this is a story all about how My " + noun1 + " got flipped turned upside down And I'd like to take a " + noun2 + ", just " + verb1 + " right there. I'll tell you how I became the " + type_of_person + " of " + place1 + "."
 
 
 # ask user for different words
 
-
+print(story)
 # show them the story
 
 
