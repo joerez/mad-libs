6fe56e4eea18c3cd9008c9fc306a917689793122
Joerez <rezenbiz@gmail.com>
2018-09-05 14:10:13 -0700

    hyperspace travel added for dramatic effect

--- madlibs.py
+++ madlibs.py
@@ -1,27 +1,14 @@
 # ask user for different words
-
-
-noun1 = input("Give me a noun!")
-noun2 = input("Give me another noun!")
-verb1 = input("Give me a verb now!")
-type_of_person = input("Give me a type of person!")
-place1 = input("Give me a place!")
+noun1 = input("Give me a noun! ")
+noun2 = input("Give me another noun! ")
+verb1 = input("Give me a verb now! ")
+type_of_person = input("Give me a type of person! ")
+place1 = input("Give me a place! ")
 
 
 # create a story here with variables as blanks
-
 story = "Now this is a story all about how My " + noun1 + " got flipped turned upside down And I'd like to take a " + noun2 + ", just " + verb1 + " right there. I'll tell you how I became the " + type_of_person + " of " + place1 + "."
 
 
 # show them the story
 print(story)
-
-
-'''
-Requirments:
-
-Users must fill out every word
-
-all user input must be strings
-
-'''
